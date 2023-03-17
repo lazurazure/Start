251258301b22c1c4a91aa7d326ef9b08140ff66c
lazurazure <lazur1380@gmail.com>
2023-03-17 17:21:41 +0200

    Homework Shildt Exercise 5.2

---
+++ src/lesson11/OverloadDemoAndFactorial.java
@@ -0,0 +1,12 @@
+package lesson11;
+
+public class OverloadDemo {
+    public static void main(String[] args) {
+
+        Overload myObj = new Overload();
+        Overload myObj1 = new Overload(3);
+        Overload myObj2 = new Overload(3, 7);
+        Overload myObj3 = new Overload(3.0,7);
+
+    }
+}
